6ad955f2113b73e348d9f95aae0e2af92df68a1c
YJL2030906598 <yjl2030906598@sjtu.edu.cn>
2023-06-25 20:45:01 +0800

    ALU updated

--- include/ALU.hpp
+++ include/ALU.hpp
@@ -4,6 +4,8 @@
 enum CalOp{
     AddOp,
     SubOp,
+    LessOp,
+    ULessOp,
     LShiftOp,
     RshiftOp,
     AndOp,
--- src/ALU.cpp
+++ src/ALU.cpp
@@ -6,6 +6,10 @@ unsigned ALU::calculate(unsigned operand1, unsigned operand2, CalOp calop){
         return operand1 + operand2;
     case SubOp:
         return operand1 - operand2;
+    case LessOp:
+        return operand1 < operand2;
+    case ULessOp:
+        return (signed)operand1 < (signed)operand2;
     case LShiftOp:
         return operand1 << operand2;
     case RshiftOp:
