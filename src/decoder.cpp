ef697c4696dbcfc384e81343b3aec9bba72235d9
YJL2030906598 <yjl2030906598@sjtu.edu.cn>
2023-06-25 10:48:27 +0800

    adjust unsigned

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -1,72 +1,72 @@
 #include "../include/decoder.hpp"
 
 void Decoder::decode(Instruction &ins){
-    if (ins.instructionBits == (unsigned)0x0ff00513){
+    if (ins.instructionBits == 0x0ff00513u){
         ins.ins_type = END;
         return;
     }
-    ins.opcode = ins.instructionBits & (unsigned)0x7f;
-    ins.rd = (ins.instructionBits >> 7) & (unsigned)0x1f;
-    ins.funct3 = (ins.instructionBits >> 12) & (unsigned)0x07;
-    ins.rs1 = (ins.instructionBits >> 15) & (unsigned)0x1f;
-    ins.rs2 = ins.shamt = (ins.instructionBits >> 20) & (unsigned)0x1f;
+    ins.opcode = ins.instructionBits & 0x7fu;
+    ins.rd = (ins.instructionBits >> 7) & 0x1fu;
+    ins.funct3 = (ins.instructionBits >> 12) & 0x07u;
+    ins.rs1 = (ins.instructionBits >> 15) & 0x1fu;
+    ins.rs2 = ins.shamt = (ins.instructionBits >> 20) & 0x1fu;
     // instruction can't match anyone is regarded as ILLEGAL for debugging
-    if (ins.opcode == (unsigned)0x37 || ins.opcode == (unsigned)0x17){
+    if (ins.opcode == 0x37u || ins.opcode == 0x17u){
         ins.ins_class = U_type;
         ins.imm = (ins.instructionBits >> 12) << 12;
-        if (ins.opcode == (unsigned)0x37)
+        if (ins.opcode == 0x37u)
             ins.ins_type = LUI;
         else
             ins.ins_type = AUIPC;
-    }else if (ins.opcode == (unsigned)0x6f){
+    }else if (ins.opcode == 0x6fu){
         ins.ins_class = J_type;
         ins.ins_type = JAL;
-        ins.imm = ins.instructionBits & (unsigned)0xff000 | (ins.instructionBits >> 9) & (unsigned)0x800 | (ins.instructionBits >> 20) & (unsigned)0x7fe | (ins.instructionBits >> 31 << 20);
-    }else if (ins.opcode == (unsigned)0x63){
+        ins.imm = ins.instructionBits & 0xff000u | (ins.instructionBits >> 9) & 0x800u | (ins.instructionBits >> 20) & 0x7feu | (ins.instructionBits >> 31 << 20);
+    }else if (ins.opcode == 0x63u){
         ins.ins_class = B_type;
-        ins.imm = (ins.instructionBits >> 7) & (unsigned)0x1e | (ins.instructionBits >> 20) & (unsigned)0x7e0 | (ins.instructionBits << 4) & (unsigned)0x800 | (ins.instructionBits >> 31 << 12);
+        ins.imm = (ins.instructionBits >> 7) & 0x1eu | (ins.instructionBits >> 20) & 0x7e0u | (ins.instructionBits << 4) & 0x800u | (ins.instructionBits >> 31 << 12);
         switch (ins.funct3){
-        case (unsigned)0x0:
+        case 0x0u:
             ins.ins_type = BEQ;
             break;
-        case (unsigned)0x1:
+        case 0x1u:
             ins.ins_type = BNE;
             break;
-        case (unsigned)0x4:
+        case 0x4u:
             ins.ins_type = BLT;
             break;
-        case (unsigned)0x5:
+        case 0x5u:
             ins.ins_type = BGE;
             break;
-        case (unsigned)0x6:
+        case 0x6u:
             ins.ins_type = BLTU;
             break;
-        case (unsigned)0x7:
+        case 0x7u:
             ins.ins_type = BGEU;
             break;
         default:
             ins.ins_type = ILLEGAL;
         }
-    }else if (ins.opcode == (unsigned)0x67 || ins.opcode == (unsigned)0x3 || ins.opcode == (unsigned)0x13){
+    }else if (ins.opcode == 0x67u || ins.opcode == 0x3u || ins.opcode == 0x13u){
         ins.ins_class = I_type;
         ins.imm = ins.instructionBits >> 20;
-        if (ins.opcode == (unsigned)0x67){
+        if (ins.opcode == 0x67u){
             ins.ins_type = JALR;
-        }else if (ins.opcode == (unsigned)0x3){
+        }else if (ins.opcode == 0x3u){
             switch (ins.funct3){
-            case (unsigned)0x0:
+            case 0x0u:
                 ins.ins_type = LB;
                 break;
-            case (unsigned)0x1:
+            case 0x1u:
                 ins.ins_type = LH;
                 break;
-            case (unsigned)0x2:
+            case 0x2u:
                 ins.ins_type = LW;
                 break;
-            case (unsigned)0x4:
+            case 0x4u:
                 ins.ins_type = LBU;
                 break;
-            case (unsigned)0x5:
+            case 0x5u:
                 ins.ins_type = LHU;
                 break;
             default:
@@ -74,34 +74,34 @@ void Decoder::decode(Instruction &ins){
             }
         }else{
             switch (ins.funct3){
-            case (unsigned)0x0:
+            case 0x0u:
                 ins.ins_type = ADDI;
                 break;
-            case (unsigned)0x2:
+            case 0x2u:
                 ins.ins_type = SLTI;
                 break;
-            case (unsigned)0x3:
+            case 0x3u:
                 ins.ins_type = SLTIU;
                 break;
-            case (unsigned)0x4:
+            case 0x4u:
                 ins.ins_type = XORI;
                 break;
-            case (unsigned)0x6:
+            case 0x6u:
                 ins.ins_type = ORI;
                 break;
-            case (unsigned)0x7:
+            case 0x7u:
                 ins.ins_type = ANDI;
                 break;
-            case (unsigned)0x1:
-                if (ins.funct7 == (unsigned)0x0)
+            case 0x1u:
+                if (ins.funct7 == 0x0u)
                     ins.ins_type = SLLI;
                 else
                     ins.ins_type = ILLEGAL;
                 break;
-            case (unsigned)0x5:
-                if (ins.funct7 == (unsigned)0x0)
+            case 0x5u:
+                if (ins.funct7 == 0x0u)
                     ins.ins_type = SRLI;
-                else if (ins.funct7 == (unsigned)0x20)
+                else if (ins.funct7 == 0x20u)
                     ins.ins_type = SRAI;
                 else
                     ins.ins_type = ILLEGAL;
@@ -110,72 +110,72 @@ void Decoder::decode(Instruction &ins){
                 ins.ins_type = ILLEGAL;
             }
         }
-    }else if (ins.opcode == (unsigned)0x23){
+    }else if (ins.opcode == 0x23u){
         ins.ins_class = S_type;
-        ins.imm = (ins.instructionBits >> 7) & (unsigned)0x1f | (ins.instructionBits >> 25 << 5);
+        ins.imm = (ins.instructionBits >> 7) & 0x1fu | (ins.instructionBits >> 25 << 5);
         switch (ins.funct3){
-        case (unsigned)0x0:
+        case 0x0u:
             ins.ins_type = SB;
             break;
-        case (unsigned)0x1:
+        case 0x1u:
             ins.ins_type = SH;
             break;
-        case (unsigned)0x2:
+        case 0x2u:
             ins.ins_type = SW;
             break;
         default:
             ins.ins_type = ILLEGAL;
         }
-    }else if (ins.opcode == (unsigned)0x33){
+    }else if (ins.opcode == 0x33u){
         switch (ins.funct3){
-        case (unsigned)0x0:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x0u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = ADD;
-            else if (ins.funct7 == (unsigned)0x20)
+            else if (ins.funct7 == 0x20u)
                 ins.ins_type = SUB;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x1:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x1u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = SLL;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x2:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x2u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = SLT;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x3:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x3u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = SLTU;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x4:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x4u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = XOR;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x5:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x5u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = SRL;
-            else if (ins.funct7 == (unsigned)0x20)
+            else if (ins.funct7 == 0x20u)
                 ins.ins_type = SRA;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x6:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x6u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = OR;
             else
                 ins.ins_type = ILLEGAL;
             break;
-        case (unsigned)0x7:
-            if (ins.funct7 == (unsigned)0x0)
+        case 0x7u:
+            if (ins.funct7 == 0x0u)
                 ins.ins_type = AND;
             else
                 ins.ins_type = ILLEGAL;
