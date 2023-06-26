23f1f181a25bcee6f1b669654028d28db2dab5ed
YJL2030906598 <yjl2030906598@sjtu.edu.cn>
2023-06-26 13:11:10 +0800

    formatting

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -49,175 +49,175 @@ void CPU::debugRun(){
         decoder.decode(ins);
         if (ins.ins_type == END) break;
         switch (ins.ins_class){
-        case U_type:{
-            switch (ins.ins_type){
-            case LUI:
-                gprs.writeVal(ins.rd, ins.imm);
-                break;
-            case AUIPC:
-                gprs.writeVal(ins.rd, pc + ins.imm);
-                break;
-            }
-            pc += 4;
-            break;
-        }
-        case J_type:{
-            gprs.writeVal(ins.rd, pc + 4);
-            pc += signedExtend_len(21, ins.imm);
-            break;
-        }
-        case B_type:{
-            bool jump = false;
-            switch (ins.ins_type){
-            case BEQ:
-                jump = (gprs.getVal(ins.rs1) == gprs.getVal(ins.rs2));
-                break;
-            case BNE:
-                jump = (gprs.getVal(ins.rs1) != gprs.getVal(ins.rs2));
-                break;
-            case BLT:
-                jump = ((signed)gprs.getVal(ins.rs1) < (signed)gprs.getVal(ins.rs2));
-                break;
-            case BGE:
-                jump = ((signed)gprs.getVal(ins.rs1) >= (signed)gprs.getVal(ins.rs2));
-                break;
-            case BLTU:
-                jump = (gprs.getVal(ins.rs1) < gprs.getVal(ins.rs2));
-                break;
-            case BGEU:
-                jump = (gprs.getVal(ins.rs1) >= gprs.getVal(ins.rs2));
+            case U_type:{
+                switch (ins.ins_type){
+                    case LUI:
+                        gprs.writeVal(ins.rd, ins.imm);
+                        break;
+                    case AUIPC:
+                        gprs.writeVal(ins.rd, pc + ins.imm);
+                        break;
+                }
+                pc += 4;
                 break;
             }
-            if (jump) pc += signedExtend_len(13, ins.imm);
-            break;
-        }
-        case I_type1:{
-            unsigned nxt_pc = pc + 4;
-            pc = (gprs.getVal(ins.rs1) + signedExtend_len(12, ins.imm)) & (-2);
-            gprs.writeVal(ins.rd, nxt_pc);
-            break;
-        }
-        case I_type2:{
-            unsigned val_to_write;
-            unsigned address_to_load = gprs.getVal(ins.rs1) + signedExtend_len(12, ins.imm);
-            unsigned len_load;
-            bool unsigned_flag = false;
-            switch (ins.ins_type){
-            case LB:
-                len_load = 1;
-                break;
-            case LH:
-                len_load = 2;
-                break;
-            case LW:
-                len_load = 4;
-                break;
-            case LBU:
-                len_load = 1;
-                unsigned_flag = true;
-                break;
-            case LHU:
-                len_load = 2;
-                unsigned_flag = true;
+            case J_type:{
+                gprs.writeVal(ins.rd, pc + 4);
+                pc += signedExtend_len(21, ins.imm);
                 break;
             }
-            memory.read(address_to_load, len_load, val_to_write);
-            if (!unsigned_flag) signedExtend_nBytes(len_load, val_to_write);
-            gprs.writeVal(ins.rd, val_to_write);
-            pc += 4;
-            break;
-        }
-        case I_type3:{
-            CalOp opi;
-            switch (ins.ins_type){
-            case ADDI:
-                opi = AddOp;
-                break;
-            case SLLI:
-                opi = LShiftOp;
-                break;
-            case SRLI:
-                opi = RshiftOp;
-                break;
-            case SRAI:
-                opi = RshiftAop;
-                break;
-            case SLTI:
-                opi = LessOp;
-                break;
-            case SLTIU:
-                opi = ULessOp;
-                break;
-            case XORI:
-                opi = XorOp;
-                break;
-            case ORI:
-                opi = OrOp;
-                break;
-            case ANDI:
-                opi = AndOp;
+            case B_type:{
+                bool jump = false;
+                switch (ins.ins_type){
+                    case BEQ:
+                        jump = (gprs.getVal(ins.rs1) == gprs.getVal(ins.rs2));
+                        break;
+                    case BNE:
+                        jump = (gprs.getVal(ins.rs1) != gprs.getVal(ins.rs2));
+                        break;
+                    case BLT:
+                        jump = ((signed)gprs.getVal(ins.rs1) < (signed)gprs.getVal(ins.rs2));
+                        break;
+                    case BGE:
+                        jump = ((signed)gprs.getVal(ins.rs1) >= (signed)gprs.getVal(ins.rs2));
+                        break;
+                    case BLTU:
+                        jump = (gprs.getVal(ins.rs1) < gprs.getVal(ins.rs2));
+                        break;
+                    case BGEU:
+                        jump = (gprs.getVal(ins.rs1) >= gprs.getVal(ins.rs2));
+                        break;
+                }
+                if (jump) pc += signedExtend_len(13, ins.imm);
                 break;
             }
-            gprs.writeVal(ins.rd, alu.calculate(gprs.getVal(ins.rs1),signedExtend_len(12,ins.imm),opi));
-            pc += 4;
-            break;
-        }
-        case R_type:{
-            CalOp op;
-            switch (ins.ins_type){
-            case ADD:
-                op = AddOp;
-                break;
-            case SUB:
-                op = SubOp;
-                break;
-            case SLL:
-                op = LShiftOp;
-                break;
-            case SRL:
-                op = RshiftOp;
-                break;
-            case SRA:
-                op = RshiftAop;
-                break;
-            case SLT:
-                op = LessOp;
-                break;
-            case SLTU:
-                op = ULessOp;
-                break;
-            case XOR:
-                op = XorOp;
-                break;
-            case OR:
-                op = OrOp;
-                break;
-            case AND:
-                op = AndOp;
+            case I_type1:{
+                unsigned nxt_pc = pc + 4;
+                pc = (gprs.getVal(ins.rs1) + signedExtend_len(12, ins.imm)) & (-2);
+                gprs.writeVal(ins.rd, nxt_pc);
                 break;
             }
-            gprs.writeVal(ins.rd, alu.calculate(gprs.getVal(ins.rs1),gprs.getVal(ins.rs2),op));
-            pc += 4;
-            break;
-        }
-        case S_type:{
-            unsigned address_to_store = gprs.getVal(ins.rs1) + signedExtend_len(12,ins.imm);
-            unsigned len_store;
-            switch (ins.ins_type){
-            case SB:
-                len_store = 1;
-                break;
-            case SH:
-                len_store = 2;
-                break;
-            case SW:
-                len_store = 4;
+            case I_type2:{
+                unsigned val_to_write;
+                unsigned address_to_load = gprs.getVal(ins.rs1) + signedExtend_len(12, ins.imm);
+                unsigned len_load;
+                bool unsigned_flag = false;
+                switch (ins.ins_type){
+                    case LB:
+                        len_load = 1;
+                        break;
+                    case LH:
+                        len_load = 2;
+                        break;
+                    case LW:
+                        len_load = 4;
+                        break;
+                    case LBU:
+                        len_load = 1;
+                        unsigned_flag = true;
+                        break;
+                    case LHU:
+                        len_load = 2;
+                        unsigned_flag = true;
+                        break;
+                }
+                memory.read(address_to_load, len_load, val_to_write);
+                if (!unsigned_flag) signedExtend_nBytes(len_load, val_to_write);
+                gprs.writeVal(ins.rd, val_to_write);
+                pc += 4;
                 break;
             }
-            memory.write(address_to_store, len_store, gprs.getVal(ins.rs2));
-            pc += 4;
-            break;
-        }
+            case I_type3:{
+                CalOp opi;
+                switch (ins.ins_type){
+                    case ADDI:
+                        opi = AddOp;
+                        break;
+                    case SLLI:
+                        opi = LShiftOp;
+                        break;
+                    case SRLI:
+                        opi = RshiftOp;
+                        break;
+                    case SRAI:
+                        opi = RshiftAop;
+                        break;
+                    case SLTI:
+                        opi = LessOp;
+                        break;
+                    case SLTIU:
+                        opi = ULessOp;
+                        break;
+                    case XORI:
+                        opi = XorOp;
+                        break;
+                    case ORI:
+                        opi = OrOp;
+                        break;
+                    case ANDI:
+                        opi = AndOp;
+                        break;
+                }
+                gprs.writeVal(ins.rd, alu.calculate(gprs.getVal(ins.rs1),signedExtend_len(12,ins.imm),opi));
+                pc += 4;
+                break;
+            }
+            case R_type:{
+                CalOp op;
+                switch (ins.ins_type){
+                    case ADD:
+                        op = AddOp;
+                        break;
+                    case SUB:
+                        op = SubOp;
+                        break;
+                    case SLL:
+                        op = LShiftOp;
+                        break;
+                    case SRL:
+                        op = RshiftOp;
+                        break;
+                    case SRA:
+                        op = RshiftAop;
+                        break;
+                    case SLT:
+                        op = LessOp;
+                        break;
+                    case SLTU:
+                        op = ULessOp;
+                        break;
+                    case XOR:
+                        op = XorOp;
+                        break;
+                    case OR:
+                        op = OrOp;
+                        break;
+                    case AND:
+                        op = AndOp;
+                        break;
+                }
+                gprs.writeVal(ins.rd, alu.calculate(gprs.getVal(ins.rs1),gprs.getVal(ins.rs2),op));
+                pc += 4;
+                break;
+            }
+            case S_type:{
+                unsigned address_to_store = gprs.getVal(ins.rs1) + signedExtend_len(12,ins.imm);
+                unsigned len_store;
+                switch (ins.ins_type){
+                    case SB:
+                        len_store = 1;
+                        break;
+                    case SH:
+                        len_store = 2;
+                        break;
+                    case SW:
+                        len_store = 4;
+                        break;
+                }
+                memory.write(address_to_store, len_store, gprs.getVal(ins.rs2));
+                pc += 4;
+                break;
+            }
         }
         gprs.writeVal(0,0);
     }
--- src/decoder.cpp
+++ src/decoder.cpp
@@ -27,26 +27,26 @@ void Decoder::decode(Instruction &ins){
         ins.ins_class = B_type;
         ins.imm = (ins.instructionBits >> 7) & 0x1eu | (ins.instructionBits >> 20) & 0x7e0u | (ins.instructionBits << 4) & 0x800u | (ins.instructionBits >> 31 << 12);
         switch (ins.funct3){
-        case 0x0u:
-            ins.ins_type = BEQ;
-            break;
-        case 0x1u:
-            ins.ins_type = BNE;
-            break;
-        case 0x4u:
-            ins.ins_type = BLT;
-            break;
-        case 0x5u:
-            ins.ins_type = BGE;
-            break;
-        case 0x6u:
-            ins.ins_type = BLTU;
-            break;
-        case 0x7u:
-            ins.ins_type = BGEU;
-            break;
-        default:
-            ins.ins_type = ILLEGAL;
+            case 0x0u:
+                ins.ins_type = BEQ;
+                break;
+            case 0x1u:
+                ins.ins_type = BNE;
+                break;
+            case 0x4u:
+                ins.ins_type = BLT;
+                break;
+            case 0x5u:
+                ins.ins_type = BGE;
+                break;
+            case 0x6u:
+                ins.ins_type = BLTU;
+                break;
+            case 0x7u:
+                ins.ins_type = BGEU;
+                break;
+            default:
+                ins.ins_type = ILLEGAL;
         }
     }else if (ins.opcode == 0x67u || ins.opcode == 0x3u || ins.opcode == 0x13u){
         ins.imm = ins.instructionBits >> 20;
@@ -56,136 +56,136 @@ void Decoder::decode(Instruction &ins){
         }else if (ins.opcode == 0x3u){
             ins.ins_class = I_type2;
             switch (ins.funct3){
-            case 0x0u:
-                ins.ins_type = LB;
-                break;
-            case 0x1u:
-                ins.ins_type = LH;
-                break;
-            case 0x2u:
-                ins.ins_type = LW;
-                break;
-            case 0x4u:
-                ins.ins_type = LBU;
-                break;
-            case 0x5u:
-                ins.ins_type = LHU;
-                break;
-            default:
-                ins.ins_type = ILLEGAL;
+                case 0x0u:
+                    ins.ins_type = LB;
+                    break;
+                case 0x1u:
+                    ins.ins_type = LH;
+                    break;
+                case 0x2u:
+                    ins.ins_type = LW;
+                    break;
+                case 0x4u:
+                    ins.ins_type = LBU;
+                    break;
+                case 0x5u:
+                    ins.ins_type = LHU;
+                    break;
+                default:
+                    ins.ins_type = ILLEGAL;
             }
         }else{
             ins.ins_class = I_type3;
             switch (ins.funct3){
-            case 0x0u:
-                ins.ins_type = ADDI;
-                break;
-            case 0x2u:
-                ins.ins_type = SLTI;
-                break;
-            case 0x3u:
-                ins.ins_type = SLTIU;
-                break;
-            case 0x4u:
-                ins.ins_type = XORI;
-                break;
-            case 0x6u:
-                ins.ins_type = ORI;
-                break;
-            case 0x7u:
-                ins.ins_type = ANDI;
-                break;
-            case 0x1u:
-                if (ins.funct7 == 0x0u)
-                    ins.ins_type = SLLI;
-                else
+                case 0x0u:
+                    ins.ins_type = ADDI;
+                    break;
+                case 0x2u:
+                    ins.ins_type = SLTI;
+                    break;
+                case 0x3u:
+                    ins.ins_type = SLTIU;
+                    break;
+                case 0x4u:
+                    ins.ins_type = XORI;
+                    break;
+                case 0x6u:
+                    ins.ins_type = ORI;
+                    break;
+                case 0x7u:
+                    ins.ins_type = ANDI;
+                    break;
+                case 0x1u:
+                    if (ins.funct7 == 0x0u)
+                        ins.ins_type = SLLI;
+                    else
+                        ins.ins_type = ILLEGAL;
+                    break;
+                case 0x5u:
+                    if (ins.funct7 == 0x0u)
+                        ins.ins_type = SRLI;
+                    else if (ins.funct7 == 0x20u)
+                        ins.ins_type = SRAI;
+                    else
+                        ins.ins_type = ILLEGAL;
+                    break;
+                default:
                     ins.ins_type = ILLEGAL;
-                break;
-            case 0x5u:
-                if (ins.funct7 == 0x0u)
-                    ins.ins_type = SRLI;
-                else if (ins.funct7 == 0x20u)
-                    ins.ins_type = SRAI;
-                else
-                    ins.ins_type = ILLEGAL;
-                break;
-            default:
-                ins.ins_type = ILLEGAL;
             }
         }
     }else if (ins.opcode == 0x23u){
         ins.ins_class = S_type;
         ins.imm = (ins.instructionBits >> 7) & 0x1fu | (ins.instructionBits >> 25 << 5);
         switch (ins.funct3){
-        case 0x0u:
-            ins.ins_type = SB;
-            break;
-        case 0x1u:
-            ins.ins_type = SH;
-            break;
-        case 0x2u:
-            ins.ins_type = SW;
-            break;
-        default:
-            ins.ins_type = ILLEGAL;
+            case 0x0u:
+                ins.ins_type = SB;
+                break;
+            case 0x1u:
+                ins.ins_type = SH;
+                break;
+            case 0x2u:
+                ins.ins_type = SW;
+                break;
+            default:
+                ins.ins_type = ILLEGAL;
         }
     }else if (ins.opcode == 0x33u){
         ins.ins_class = R_type;
         switch (ins.funct3){
-        case 0x0u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = ADD;
-            else if (ins.funct7 == 0x20u)
-                ins.ins_type = SUB;
-            else
+            case 0x0u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = ADD;
+                else if (ins.funct7 == 0x20u)
+                    ins.ins_type = SUB;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x1u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = SLL;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x2u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = SLT;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x3u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = SLTU;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x4u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = XOR;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x5u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = SRL;
+                else if (ins.funct7 == 0x20u)
+                    ins.ins_type = SRA;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x6u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = OR;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            case 0x7u:
+                if (ins.funct7 == 0x0u)
+                    ins.ins_type = AND;
+                else
+                    ins.ins_type = ILLEGAL;
+                break;
+            default:
                 ins.ins_type = ILLEGAL;
-            break;
-        case 0x1u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = SLL;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        case 0x2u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = SLT;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        case 0x3u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = SLTU;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        case 0x4u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = XOR;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        case 0x5u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = SRL;
-            else if (ins.funct7 == 0x20u)
-                ins.ins_type = SRA;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        case 0x6u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = OR;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        case 0x7u:
-            if (ins.funct7 == 0x0u)
-                ins.ins_type = AND;
-            else
-                ins.ins_type = ILLEGAL;
-            break;
-        default:
-            ins.ins_type = ILLEGAL;
         }
     }else{
         ins.ins_type = ILLEGAL;
